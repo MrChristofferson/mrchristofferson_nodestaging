6590321a4863286f7f47883143d67fb7fcdbe474
Chris Vandenberg <chrisvan0848@gmail.com>
2016-06-15 14:19:34 -0700

    keep on keepin on

--- index.js
+++ index.js
@@ -3,18 +3,24 @@ var app = express();
 
 app.set('port', (process.env.PORT || 5000));
 
-app.use(express.static(__dirname + '/public'));
+// app.use(express.static(__dirname + '/public'));
 
 // views is directory for all template files
-app.set('views', __dirname + '/views');
-app.set('view engine', 'ejs');
+// app.set('views', __dirname + '/views');
+// app.set('view engine', 'ejs');
 
 app.get('/', function(request, response) {
-  response.render('pages/index');
+  response.sendFile(__dirname + '\\index.html');
+});
+
+app.get('/about', function(request, response) {
+  response.sendFile(__dirname + '\\about.html');
+});
+
+app.get('/other', function(request, response) {
+  response.sendFile(__dirname + '\\other.html');
 });
 
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
 });
-
-
