f06d117806d032fbb6a5c28ddbc9694672b56fb1
Chris Vandenberg <chrisvan0848@gmail.com>
2016-06-15 13:57:03 -0700

    testing

--- server.js
+++ server.js
@@ -5,4 +5,12 @@ app.get('/', (req, res) => {
   res.sendFile(`${__dirname}\\index.html`);
 });
 
+app.get('/about', (req, res) => {
+  res.sendFile(`${__dirname}\\about.html`);
+});
+
+app.get('/other', (req, res) => {
+  res.sendFile(`${__dirname}\\other.html`);
+});
+
 app.listen(3002);
