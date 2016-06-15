ec52f9cfcfbf8e919c53da508403fafa8315d53a
Chris Vandenberg <chrisvan0848@gmail.com>
2016-06-15 14:04:31 -0700

    humble myself

--- index.js
+++ index.js
@@ -1,16 +1,20 @@
 var express = require('express');
 var app = express();
 
-app.get('/', (req, res) => {
-  res.sendFile(`${__dirname}\\index.html`);
+app.set('port', (process.env.PORT || 5000));
+
+app.use(express.static(__dirname + '/public'));
+
+// views is directory for all template files
+app.set('views', __dirname + '/views');
+app.set('view engine', 'ejs');
+
+app.get('/', function(request, response) {
+  response.render('pages/index');
 });
 
-app.get('/about', (req, res) => {
-  res.sendFile(`${__dirname}\\about.html`);
+app.listen(app.get('port'), function() {
+  console.log('Node app is running on port', app.get('port'));
 });
 
-app.get('/other', (req, res) => {
-  res.sendFile(`${__dirname}\\other.html`);
-});
 
-app.listen(3002);
